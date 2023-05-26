2ff0f015e16ebe0e96b0ad35db47fe4c6041e786
Guilherme Padovine de Castro <guipadocastro@gmail.com>
2023-05-25 21:43:34 -0300

    Funcionando com dashboard padrão

--- cons_dentario/routes/web.php
+++ cons_dentario/routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
